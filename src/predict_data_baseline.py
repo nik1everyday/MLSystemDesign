a3b70fcc230152654e264807d6576d3d008b0529
Nikita Borisov <nik-everyday@yandex.ru>
2023-11-19 14:17:37 +0300

    backend-dev: Update function for forecasting

--- src/predict_data_baseline.py
+++ src/predict_data_baseline.py
@@ -1,29 +1,24 @@
+from datetime import datetime, timedelta
+import pandas as pd
 from statsmodels.tsa.api import SimpleExpSmoothing
+
 from src.load_data import load_oil_price_data
-import numpy as np
 
 
-# Достанем исторические данные
-start_date = "2023-03-20"  # Задайте дату начала в формате "гггг-мм-дд"
-oil_data = load_oil_price_data(start_date)
-train_data = oil_data['Close']
+# Модифицированная функция прогнозирования
+def predict_data(start_date: str, forecast_days: int):
+    # Загрузка данных за последние 6 месяцев до start_date
+    start_date_datetime = datetime.strptime(start_date, "%Y-%m-%d")
+    six_months_ago = start_date_datetime - timedelta(days=180)
+    oil_data = load_oil_price_data(six_months_ago.strftime("%Y-%m-%d"))  # Использование функции загрузки данных
+    train_data = oil_data['Close']
 
+    # Обучение модели
+    smoothing_level = 0.9999999850987182
+    model = SimpleExpSmoothing(train_data, initialization_method="heuristic").fit(smoothing_level=smoothing_level, optimized=False)
 
-# Подготовим модель
-smoothing_level = 0.9999999850987182
-model_exp_smoothing = SimpleExpSmoothing(train_data, initialization_method="heuristic").fit(
-    smoothing_level=smoothing_level, optimized=False
-)
-
-
-def predict_exp_smoothing_next_day(model):
-    """
-    Возвращает прогноз цены закрытия на следующий день
-
-    :return: float цена на следующий день
-    """
-    try:
-        return model.forecast(1).values[0]
-    except Exception as ex:
-        print(f'Произошла ошибка \n{ex}')
-        return np.nan
+    # Прогнозирование на указанное количество дней
+    forecast = model.forecast(forecast_days)
+    forecast_dates = pd.date_range(start=start_date, periods=forecast_days, freq='D')
+    forecast_df = pd.DataFrame({'Date': forecast_dates, 'Predicted_Close': forecast.values})
+    return forecast_df
