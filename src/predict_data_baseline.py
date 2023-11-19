ab6f5aa10e97225849f8a4cdcfa4d89903eb6de9
Nikita Borisov <nik-everyday@yandex.ru>
2023-11-19 14:18:11 +0300

    backend-dev: Update function for forecasting

--- src/predict_data_baseline.py
+++ src/predict_data_baseline.py
@@ -8,7 +8,7 @@ from src.load_data import load_oil_price_data
 def predict_data(start_date: str, forecast_days: int):
     start_date_datetime = datetime.strptime(start_date, "%Y-%m-%d")
     six_months_ago = start_date_datetime - timedelta(days=180)
-    oil_data = load_oil_price_data(six_months_ago.strftime("%Y-%m-%d"))  # Использование функции загрузки данных
+    oil_data = load_oil_price_data(six_months_ago.strftime("%Y-%m-%d"))
     train_data = oil_data['Close']
 
     smoothing_level = 0.9999999850987182
