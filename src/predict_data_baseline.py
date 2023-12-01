89f187c4f254db93de5f2d43d475d472495c0ae2
Nikita Borisov <nik-everyday@yandex.ru>
2023-12-01 22:36:04 +0300

    dev: Divide prediction function into two: train and predict

--- src/predict_data_baseline.py
+++ src/predict_data_baseline.py
@@ -1,36 +1,46 @@
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import pandas as pd
 from prophet import Prophet
 
 from src.load_data import load_oil_price_data
 
 
-def predict_data(start_date: str, forecast_days: int):
-    logging.basicConfig(level=logging.INFO)
+def train_model():
 
-    start_date_datetime = datetime.strptime(start_date, "%Y-%m-%d")
-    six_months_ago = start_date_datetime - timedelta(days=180)
+    current_date_str = datetime.now().strftime("%Y-%m-%d")
+    current_date = datetime.strptime(current_date_str, "%Y-%m-%d")
 
-    oil_data = load_oil_price_data(six_months_ago.strftime("%Y-%m-%d"))
+    three_months_earlier = current_date - relativedelta(months=3)
+    three_months_earlier_str = three_months_earlier.strftime("%Y-%m-%d")
 
-    oil_data.dropna(inplace=True)
+    oil_data = load_oil_price_data(three_months_earlier_str)
+
     train_data = pd.DataFrame(data=oil_data['Close'], index=oil_data.index)
     train_data.reset_index(inplace=True)
     train_data.rename(columns={'Date': 'ds', 'Close': 'y'}, inplace=True)
     train_data['ds'] = train_data['ds'].dt.tz_localize(None)
 
     try:
-        model = Prophet(yearly_seasonality=True, daily_seasonality=True, seasonality_mode='multiplicative')
+        model = Prophet(yearly_seasonality=True,
+                        weekly_seasonality=True,
+                        daily_seasonality=True,
+                        seasonality_mode='multiplicative')
         model.fit(train_data)
+        return model
     except Exception as e:
         logging.error(f"An error occurred during model fitting: {e}")
-        return None
+        raise
+
+
+def predict_data(forecast_days: int, model):
+    current_date_str = datetime.now().strftime("%Y-%m-%d")
 
     future = model.make_future_dataframe(periods=forecast_days, freq='d')
     forecast = model.predict(future)
 
-    forecast_dates = pd.date_range(start=start_date, periods=forecast_days, freq='D')
+    forecast_dates = pd.date_range(start=current_date_str, periods=forecast_days, freq='D')
     forecast_df = pd.DataFrame({'Date': forecast_dates, 'Predicted_Close': forecast.yhat[-forecast_days:]})
 
     return forecast_df
